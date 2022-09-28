9b35bbbeb6217a24493da38780fc012da9779593
David Redding <redding.david35@gmail.com>
2022-09-28 10:17:24 -0500

    Main React-Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './pages/dashboard/Dashboard';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Create from './pages/create/Create';
+import Project from './pages/project/Project';
+
 const App = () => (
-  <div>
-    <h1 className="font-poppins">Hi</h1>
+  <div className="flex">
+    <Router>
+      <div className="bg-blue-300 px-14 grow">
+        {/* Navbar Component */}
+        <Switch>
+          <Route exact path="/">
+            <Dashboard />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/project/:id">
+            <Project />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   </div>
 );
 
